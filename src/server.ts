a6a434589b9b1d0cff722633eb5e99ee79380ac9
goren.ali@yandex.com <goren.ali@yandex.com>
2019-09-28 15:36:55 +0300

    first commit

---
+++ src/server.ts
@@ -0,0 +1,23 @@
+import App from './app'
+
+import * as bodyParser from 'body-parser'
+import loggerMiddleware from './middleware/logger'
+
+import PostsController from './controllers/posts/posts.controller'
+
+const app = new App({
+    port: 5000,
+    controllers: [
+        new PostsController()
+    ],
+    middleWares: [
+        bodyParser.json(),
+        bodyParser.urlencoded({ extended: true }),
+        loggerMiddleware
+    ]
+})
+
+app.listen()
+
+// http://rousseau-alexandre.fr/en/programming/2019/06/19/express-typescript.html
+// https://wanago.io/2018/12/03/typescript-express-tutorial-routing-controllers-middleware/
